e18d7e2ad850d44414204683b391c6d22f6a5afe
Claudio Muse <cemuse@hotmail.com.ar>
2023-03-21 22:37:55 -0300

    Trabajo para Argentina Programa
    CV realizado con HTML, CSS y JS para entrega de trabajo final para Argentina Programa

---
+++ js/cv.js
@@ -0,0 +1,24 @@
+
+/* Funciones de los iconos */
+
+
+/* Funcion de los botones */
+
+function mostrar(){
+    document.getElementById("div-a").style.display = "block";
+   
+}
+
+function mostrar2(){
+    document.getElementById("div-b").style.display = "block";
+   
+}
+
+function ocultar(){
+    document.getElementById("div-a").style.display = "none";
+
+}
+
+function ocultar2(){
+    document.getElementById("div-b").style.display = "none";
+}
